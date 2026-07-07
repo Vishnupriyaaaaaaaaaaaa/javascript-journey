a1dd8c9e52df35b851378ce05b5a269e98ec3b96
Vishnupriya R <vpvpvp6092@gmail.com>
2026-07-07 17:47:50 +0530

    Create remove duplicates.js

---
+++ remove duplicates.js
@@ -0,0 +1,12 @@
+//remove duplicate elements
+let arr=[4,3,2,1,1,9,1,4];
+let result=[];
+for(let i=0;i<arr.length;i++){
+  if(result.includes(arr[i])){
+  }else{
+      result.push(arr[i]);
+      
+  
+  } 
+}
+console.log(result);
